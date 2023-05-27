30d326251e365beec2c9e4b4bd176c112f6966dc
Jsbist21 <bishtjatin477@gmail.com>
2023-05-27 11:57:16 +0530

    Fetch games with react query

--- src/Services/api-client.ts
+++ src/Services/api-client.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        
-        key: '0f4275f3e92145c8a5354c1ba3b4f6eb'
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
-    }
-})
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "0f4275f3e92145c8a5354c1ba3b4f6eb",
+  },
+});
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,6 @@
-import { useEffect, useState } from "react";
-import apiClient from "../Services/api-client";
-import { CanceledError } from "axios";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import apiClient, { FetchResponse } from "../Services/api-client";
 import { GameQuery } from "../App";
+import { useQuery } from "@tanstack/react-query";
 
 export interface Platform {
   id: number;
@@ -21,17 +18,19 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 
 export default useGames;
